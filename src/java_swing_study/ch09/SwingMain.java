cadbbf058ff8b8f05b5a6037329b7a0b3e8d4c13
jsonhyun <airplant02342@hotmail.com>
2020-01-07 15:20:13 +0900

    예제 9-2 추가

--- src/java_swing_study/ch09/SwingMain.java
+++ src/java_swing_study/ch09/SwingMain.java
@@ -1,10 +1,23 @@
 package java_swing_study.ch09;
 
+import java.awt.EventQueue;
+
 public class SwingMain {
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		MyFrame mf = new MyFrame();
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ContentPaneExcercise frame = new ContentPaneExcercise();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
