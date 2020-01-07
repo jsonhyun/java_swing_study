1fab35d6185fad139ea50f019243cfc4c19d349b
jsonhyun <airplant02342@hotmail.com>
2020-01-07 16:27:27 +0900

    라인 추가

--- src/java_swing_study/ch09/ContentPaneExcercise.java
+++ src/java_swing_study/ch09/ContentPaneExcercise.java
@@ -4,21 +4,70 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
 
-public class ContentPaneExcercise extends JFrame {
+public class ContentPaneExcercise extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton btnOK;
+	private JButton btnCancel;
+	private JButton btnIgnore;
 
 	
 	public ContentPaneExcercise() {
+		initialize();
+	}
+	private void initialize() {
+		setTitle("ContentPane과 JFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.ORANGE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnOK = new JButton("OK");
+		btnOK.addActionListener(this);
+		contentPane.add(btnOK);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(this);
+		contentPane.add(btnCancel);
+		
+		btnIgnore = new JButton("Ignore");
+		btnIgnore.addActionListener(this);
+		contentPane.add(btnIgnore);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnIgnore) {
+			btnIgnoreActionPerformed(e);
+		}
+		if (e.getSource() == btnCancel) {
+			btnCancelActionPerformed(e);
+		}
+		if (e.getSource() == btnOK) {
+			btnOKActionPerformed(e);
+		}
+	}
+	protected void btnOKActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnCancelActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnIgnoreActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
 }
