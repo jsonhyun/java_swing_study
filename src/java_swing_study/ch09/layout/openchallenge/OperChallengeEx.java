fece3deb4179f0ccf02aa3a045bcb79dbbbc3db8
jsonhyun <airplant@naver.com>
2020-01-08 14:51:25 +0900

    import 정리 및 레이아웃 메인 버튼 추가

--- src/java_swing_study/ch09/layout/openchallenge/OperChallengeEx.java
+++ src/java_swing_study/ch09/layout/openchallenge/OperChallengeEx.java
@@ -1,7 +1,6 @@
 package java_swing_study.ch09.layout.openchallenge;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,31 +13,13 @@ public class OperChallengeEx extends JFrame {
 	private NorthPanel pNorth;
 	private CenterPanel pCenter;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					OperChallengeEx frame = new OperChallengeEx();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public OperChallengeEx() {
 		initialize();
 	}
 	private void initialize() {
 		setTitle("간단한 스윙 응용프로그램");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
