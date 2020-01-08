3338de794975ad5005aa5d502e69f654b804035c
jsonhyun <airplant@naver.com>
2020-01-08 17:52:59 +0900

    레이아웃 메인에 실습문제 버튼 추가

--- src/java_swing_study/ch09/layout/LayoutMain.java
+++ src/java_swing_study/ch09/layout/LayoutMain.java
@@ -1,26 +1,42 @@
 package java_swing_study.ch09.layout;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 
 import java_swing_study.ch09.layout.openchallenge.OperChallengeEx;
+import java_swing_study.ch09.layout.silsup02.ButtonEx;
+import java_swing_study.ch09.layout.silsup04.TenColorButtonFrameEx;
+import java_swing_study.ch09.layout.silsup05.ColorFrameEx;
+import java_swing_study.ch09.layout.silsup07.OperatorEx;
+import java_swing_study.ch09.layout.silsup08.StarFrameEx;
 
 @SuppressWarnings("serial")
 public class LayoutMain extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JButton btnFlowLayout;
-	private JButton btnBorderLayout;
-	private JButton btnNewButton;
-	private JButton btnAbsoluteLayout;
+	private JPanel pLeft;
+	private JButton btnFlow;
+	private JButton btnBorder;
+	private JButton btnGrid;
+	private JButton btnAbsolute;
 	private JButton btnOpenChallenge;
+	private JPanel pRight;
+	private JButton btnPracticeNo2;
+	private JButton btnPracticeNo4;
+	private JButton btnPracticeNo5;
+	private JButton btnPracticeNo7;
+	private JButton btnPracticeNo8;
 
 	/**
 	 * Launch the application.
@@ -47,68 +63,145 @@ public class LayoutMain extends JFrame implements ActionListener {
 	private void initialize() {
 		setTitle("배치레이아웃예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 200);
+		setBounds(100, 100, 611, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new TitledBorder(null, "레이아웃 예제", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\uC81C9\uC7A5 GUI \uAE30\uCD08", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 2, 10, 10));
 		
-		btnFlowLayout = new JButton("FlowLayout");
-		btnFlowLayout.addActionListener(this);
-		contentPane.add(btnFlowLayout);
+		pLeft = new JPanel();
+		pLeft.setBorder(new TitledBorder(null, "\uB808\uC774\uC544\uC6C3 \uC608\uC81C", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(pLeft);
+		pLeft.setLayout(new GridLayout(0, 2, 10, 10));
 		
-		btnBorderLayout = new JButton("BorderLayout");
-		btnBorderLayout.addActionListener(this);
-		contentPane.add(btnBorderLayout);
+		btnFlow = new JButton("FlowLayout");
+		btnFlow.addActionListener(this);
+		pLeft.add(btnFlow);
 		
-		btnNewButton = new JButton("GridLayout");
-		btnNewButton.addActionListener(this);
-		contentPane.add(btnNewButton);
+		btnBorder = new JButton("BorderLayout");
+		btnBorder.addActionListener(this);
+		pLeft.add(btnBorder);
 		
-		btnAbsoluteLayout = new JButton("AbsoluteLayout");
-		btnAbsoluteLayout.addActionListener(this);
-		contentPane.add(btnAbsoluteLayout);
+		btnGrid = new JButton("GridLayout");
+		btnGrid.addActionListener(this);
+		pLeft.add(btnGrid);
+		
+		btnAbsolute = new JButton("AbsoluteLayout");
+		btnAbsolute.addActionListener(this);
+		pLeft.add(btnAbsolute);
 		
 		btnOpenChallenge = new JButton("OpenChallenge");
 		btnOpenChallenge.addActionListener(this);
-		contentPane.add(btnOpenChallenge);
+		pLeft.add(btnOpenChallenge);
+		
+		pRight = new JPanel();
+		pRight.setBorder(new TitledBorder(null, "\uC2E4\uC2B5\uBB38\uC81C \uD480\uC774", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(pRight);
+		pRight.setLayout(new GridLayout(0, 2, 10, 10));
+		
+		btnPracticeNo2 = new JButton("실습문제 2번");
+		btnPracticeNo2.addActionListener(this);
+		pRight.add(btnPracticeNo2);
+		
+		btnPracticeNo4 = new JButton("실습문제 4번");
+		btnPracticeNo4.addActionListener(this);
+		pRight.add(btnPracticeNo4);
+		
+		btnPracticeNo5 = new JButton("실습문제 5번");
+		btnPracticeNo5.addActionListener(this);
+		pRight.add(btnPracticeNo5);
+		
+		btnPracticeNo7 = new JButton("실습문제 7번");
+		btnPracticeNo7.addActionListener(this);
+		pRight.add(btnPracticeNo7);
+		
+		btnPracticeNo8 = new JButton("실습문제 8번");
+		btnPracticeNo8.addActionListener(this);
+		btnPracticeNo8.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				System.out.println(e.getButton() + ":" + e.getClickCount());
+			}
+			
+		});
+		pRight.add(btnPracticeNo8);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getActionCommand());
+//		JOptionPane.showMessageDialog(null, e.getSource());
+		
+		if (e.getSource() == btnPracticeNo8) {
+			btnPracticeNo8ActionPerformed(e);
+		}
+		if (e.getSource() == btnPracticeNo7) {
+			btnPracticeNo7ActionPerformed(e);
+		}
+		if (e.getSource() == btnPracticeNo5) {
+			btnPracticeNo5ActionPerformed(e);
+		}
+		if (e.getSource() == btnPracticeNo4) {
+			btnPracticeNo4ActionPerformed(e);
+		}
 		if (e.getSource() == btnOpenChallenge) {
 			btnOpenChallengeActionPerformed(e);
 		}
-		if (e.getSource() == btnAbsoluteLayout) {
-			btnAbsoluteLayoutActionPerformed(e);
+		if (e.getSource() == btnAbsolute) {
+			btnAbsoluteActionPerformed(e);
 		}
-		if (e.getSource() == btnNewButton) {
-			btnNewButtonActionPerformed(e);
+		if (e.getSource() == btnGrid) {
+			btnGridActionPerformed(e);
 		}
-		if (e.getSource() == btnBorderLayout) {
-			btnBorderLayoutActionPerformed(e);
+		if (e.getSource() == btnBorder) {
+			btnBorderActionPerformed(e);
 		}
-		if (e.getSource() == btnFlowLayout) {
-			btnFlowLayoutActionPerformed(e);
+		if (e.getSource() == btnFlow) {
+			btnFlowActionPerformed(e);
+		}
+		if (e.getSource() == btnPracticeNo2) {
+			btnPracticeNo2ActionPerformed(e);
 		}
 	}
-	protected void btnFlowLayoutActionPerformed(ActionEvent e) {
+	protected void btnPracticeNo2ActionPerformed(ActionEvent e) {
+		ButtonEx frame = new ButtonEx();
+		frame.setVisible(true);
+	}
+	protected void btnFlowActionPerformed(ActionEvent e) {
 		FlowLayoutEx frame = new FlowLayoutEx();
 		frame.setVisible(true);
 	}
-	protected void btnBorderLayoutActionPerformed(ActionEvent e) {
+	protected void btnBorderActionPerformed(ActionEvent e) {
 		BorderLayoutEx frame = new BorderLayoutEx();
 		frame.setVisible(true);
 	}
-	protected void btnNewButtonActionPerformed(ActionEvent e) {
+	protected void btnGridActionPerformed(ActionEvent e) {
 		GridLayoutEx frame = new GridLayoutEx();
 		frame.setVisible(true);
 	}
-	protected void btnAbsoluteLayoutActionPerformed(ActionEvent e) {
+	protected void btnAbsoluteActionPerformed(ActionEvent e) {
 		AbsoluteLayoutEx frame = new AbsoluteLayoutEx();
 		frame.setVisible(true);
 	}
 	protected void btnOpenChallengeActionPerformed(ActionEvent e) {
 		OperChallengeEx frame = new OperChallengeEx();
+		frame.setVisible(true);
+	}
+	protected void btnPracticeNo4ActionPerformed(ActionEvent e) {
+		TenColorButtonFrameEx frame = new TenColorButtonFrameEx();
+		frame.setVisible(true);
+	}
+	protected void btnPracticeNo5ActionPerformed(ActionEvent e) {
+		ColorFrameEx frame = new ColorFrameEx();
+		frame.setVisible(true);
+	}
+	protected void btnPracticeNo7ActionPerformed(ActionEvent e) {
+		OperatorEx frame = new OperatorEx();
+		frame.setVisible(true);
+	}
+	protected void btnPracticeNo8ActionPerformed(ActionEvent e) {
+		StarFrameEx frame = new StarFrameEx();
 		frame.setVisible(true);
 	}
 }
